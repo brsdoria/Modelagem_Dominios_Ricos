ee3aa7cc7820c93278360ba35594f30339b67fa7
Byron Doria <brsdoria@gmail.com>
2024-12-21 14:52:47 -0300

    commit realizado

--- src/EventSourcing/obj/Debug/netcoreapp2.2/EventSourcing.AssemblyInfo.cs
+++ src/EventSourcing/obj/Debug/netcoreapp2.2/EventSourcing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventSourcing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa8b0f3b1cb97392197d48972a86ca335ce636c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4588cb7f26580e540e2375f8da9924b10231233b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventSourcing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventSourcing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
