9f9a8acef2993b605addf3200e7a0274092a0a30
Byron Doria <brsdoria@gmail.com>
2024-12-21 14:37:03 -0300

    commit realizado

--- tests/NerdStore.Catalogo.Domain.Tests/obj/Debug/netcoreapp2.2/NerdStore.Catalogo.Domain.Tests.AssemblyInfo.cs
+++ tests/NerdStore.Catalogo.Domain.Tests/obj/Debug/netcoreapp2.2/NerdStore.Catalogo.Domain.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NerdStore.Catalogo.Domain.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da625e4b24b52275f5de555b27207fe2c2048284")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe89a8591236429d3f6812c7b4619beb738ef762")]
 [assembly: System.Reflection.AssemblyProductAttribute("NerdStore.Catalogo.Domain.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NerdStore.Catalogo.Domain.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
